25689ec483be08c0c125bbe54108cedf161d2111
lame <lamesgindesalegn30@gmail.com>
2025-05-12 11:40:02 -0700

    I started backend

---
+++ Backend/index.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const authRoutes = require("./routes/authRoutes");
+const doctorRoutes = require("./routes/doctorRoutes");
+const triageRoutes = require("./routes/triageRoutes");
+const adminRoutes = require("./routes/adminRoutes");
+const patientRoutes = require("./routes/patientRoutes");
