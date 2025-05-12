22098dab6f2c8bb6737626b0412bebd5534898db
lame <lamesgindesalegn30@gmail.com>
2025-05-12 11:41:37 -0700

    updated backend

--- Backend/index.js
+++ Backend/index.js
@@ -13,3 +13,11 @@ const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
