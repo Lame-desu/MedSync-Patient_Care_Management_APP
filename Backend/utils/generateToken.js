db3c9c0dedcc5d141cf46b6eae8b1e6d4978b1c7
Blen Redwan <163293320+blen3696@users.noreply.github.com>
2025-05-12 20:29:55 +0300

    Update generateToken.js

--- Backend/utils/generateToken.js
+++ Backend/utils/generateToken.js
@@ -1,8 +1,8 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-const JWT_SECRET = process.env.JWT_SECRET;
 const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || '10h';
+const JWT_SECRET = process.env.JWT_SECRET;
 
 const generateToken = (userId, role) => {
   return jwt.sign({ id: userId, role }, JWT_SECRET, {
